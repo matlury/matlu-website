837fb8eca1c4ee4ae53216259de8a4afad77e027
alehuo <aleksi.huotala@helsinki.fi>
2020-03-06 13:29:30 +0200

    Add Thank you page

---
+++ src/pages/thank-you.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import { SEO } from "../seo"
+
+const ThankYou: React.FC = () => {
+  return (
+    <Layout
+      language="en"
+      localizedLinks={{
+        fi: "/",
+        en: "/en/",
+      }}
+    >
+      <SEO
+        title={"Kiitos yhteydenotostasi! / Thank you for contacting!"}
+        description={"Kiitos yhteydenotostasi! / Thank you for contacting!"}
+        lang="fi"
+        hideFromSearchEngine={true}
+      />
+      <h1>Kiitos yhteydenotostasi! / Thank you for contacting!</h1>
+    </Layout>
+  )
+}
+
+export default ThankYou
