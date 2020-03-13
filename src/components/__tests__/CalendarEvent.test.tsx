c2ae6ccc3b5954ad66b3b94cbcb556c4413bba81
alehuo <aleksi.huotala@helsinki.fi>
2020-03-13 17:18:15 +0200

    Update Gatsby. Enable testing

---
+++ src/components/__tests__/CalendarEvent.test.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import CalendarEvent from "../CalendarEvent"
+
+describe(`CalendarEvent`, () => {
+  it(`renders siteTitle`, () => {
+    const title_fi = "Testi"
+    const title_en = "Test"
+    const loc = "Helsinki"
+    const startDate = "2020-03-13T15:00:00.000Z"
+    const { getByText } = render(
+      <CalendarEvent
+        event_link="https://example.com"
+        hide_location
+        language="fi"
+        start_date={startDate}
+        title={{
+          en: title_en,
+          fi: title_fi,
+        }}
+        location={{
+          en: loc,
+          fi: loc,
+        }}
+      />
+    )
+
+    const titleFi = getByText(title_fi)
+    expect(titleFi).toBeInTheDocument()
+  })
+})
