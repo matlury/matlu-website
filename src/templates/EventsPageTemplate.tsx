9cb3649701a425901a7252d23de9e0b96ff90304
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2020-12-16 12:21:47 +0200

    fix: Missing import

--- src/templates/EventsPageTemplate.tsx
+++ src/templates/EventsPageTemplate.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import { SEO } from "../seo";
