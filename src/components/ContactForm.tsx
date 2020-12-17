2560031aba1e386e2ce2aff65db52dabdd0b119f
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2020-12-17 17:14:29 +0200

    fix: Recaptcha

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./ContactForm.module.scss";
 import { Language } from "../utils";
-import Recaptcha from "react-recaptcha";
 import { graphql, useStaticQuery } from "gatsby";
+import Reaptcha from "reaptcha";
 interface ContactFormProps {
   lang: Language;
 }
@@ -15,73 +15,146 @@ interface ContactFormFragmentProps {
 const ContactFormFi: React.FC<ContactFormFragmentProps> = ({
   reCaptchaSiteKey,
   feedbackFormHandler,
-}) => (
-  <section>
-    <h1>Yhteydenottolomake</h1>
-    <p>
-      Yhteydenottolomake on anonyymi, ja välitetään Matlun hallitukselle
-      sähköpostitse. Voit halutessasi jättää viestiin yhteystietosi, jos haluat
-      vastauksen yhteydenottoosi.
-    </p>
-    <form
-      action={feedbackFormHandler}
-      method="POST"
-      className={styles.contactForm}
-    >
-      <div className={styles.contactFormGroup}>
-        <label htmlFor="contactmsg">Viesti</label>
-        <textarea
-          id="contactmsg"
-          name="message"
-          cols={80}
-          rows={10}
-          placeholder="Kirjoita viestisi..."
-        />
-      </div>
-      <div className={styles.contactFormGroup}>
-        <Recaptcha sitekey={reCaptchaSiteKey} />
-      </div>
-      <div className={styles.contactFormGroup}>
-        <button type="submit">Lähetä</button>
-      </div>
-    </form>
-  </section>
-);
+}) => {
+  const [verified, setVerified] = useState(false);
+  const [js, setJs] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [response, setResponse] = useState("");
+  const onLoad = useCallback(() => {
+    setLoaded(true);
+  }, [setLoaded]);
+  useEffect(() => {
+    setJs(true);
+    return () => {
+      setResponse("");
+      setVerified(false);
+    };
+  }, []);
+  return (
+    <section>
+      <h1>Yhteydenottolomake</h1>
+      <p>
+        Yhteydenottolomake on anonyymi, ja välitetään Matlun hallitukselle
+        sähköpostitse. Voit halutessasi jättää viestiin yhteystietosi, jos
+        haluat vastauksen yhteydenottoosi.
+      </p>
+      {!js && (
+        <p>
+          <strong>
+            Selaimessasi täytyy olla JavaScript-tuki päällä, jotta voit lähettää
+            viestin.
+          </strong>
+        </p>
+      )}
+      {js && (
+        <form
+          action={feedbackFormHandler}
+          method="POST"
+          className={styles.contactForm}
+        >
+          <div className={styles.contactFormGroup}>
+            <label htmlFor="contactmsg">Viesti</label>
+            <textarea
+              id="contactmsg"
+              name="message"
+              cols={80}
+              rows={10}
+              placeholder="Kirjoita viestisi..."
+            />
+          </div>
+          <div className={styles.contactFormGroup}>
+            <Reaptcha
+              sitekey={reCaptchaSiteKey}
+              onVerify={(response) => {
+                setVerified(true);
+                setResponse(response);
+              }}
+              onLoad={onLoad}
+            />
+          </div>
+          {response !== "" && (
+            <input type="hidden" name="g-recaptcha-response" value={response} />
+          )}
+          <div className={styles.contactFormGroup}>
+            <button type="submit" disabled={!loaded || !verified}>
+              Lähetä
+            </button>
+          </div>
+        </form>
+      )}
+    </section>
+  );
+};
 
 const ContactFormEn: React.FC<ContactFormFragmentProps> = ({
   reCaptchaSiteKey,
   feedbackFormHandler,
-}) => (
-  <section>
-    <h1>Contact form</h1>
-    <p>
-      The contact form is anonymous, and will be sent to the board of Matlu via
-      email. Optionally, you can choose to leave your contact information, if
-      you want an answer to your contact request.
-    </p>
-    <form
-      action={feedbackFormHandler}
-      method="POST"
-      className={styles.contactForm}
-    >
-      <div className={styles.contactFormGroup}>
-        <label htmlFor="contactmsg">Message</label>
-        <textarea
-          id="contactmsg"
-          cols={80}
-          rows={10}
-          placeholder="Write your message..."
-        />
-      </div>
-      <div className={styles.contactFormGroup}>
-        <Recaptcha sitekey={reCaptchaSiteKey} />
-      </div>
-      <div className={styles.contactFormGroup}>
-        <button type="submit">Send</button>
-      </div>
-    </form>
-  </section>
-);
+}) => {
+  const [verified, setVerified] = useState(false);
+  const [js, setJs] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [response, setResponse] = useState("");
+  const onLoad = useCallback(() => {
+    setLoaded(true);
+  }, [setLoaded]);
+  useEffect(() => {
+    setJs(true);
+    return () => {
+      setResponse("");
+      setVerified(false);
+    };
+  }, []);
+  return (
+    <section>
+      <h1>Contact form</h1>
+      <p>
+        The contact form is anonymous, and will be sent to the board of Matlu
+        via email. Optionally, you can choose to leave your contact information,
+        if you want an answer to your contact request.{" "}
+      </p>
+      {!js && (
+        <p>
+          <strong>The form requires JavaScript support to be enabled.</strong>
+        </p>
+      )}
+      {js && (
+        <form
+          action={feedbackFormHandler}
+          method="POST"
+          className={styles.contactForm}
+        >
+          <div className={styles.contactFormGroup}>
+            <label htmlFor="contactmsg">Message</label>
+            <textarea
+              id="contactmsg"
+              cols={80}
+              rows={10}
+              placeholder="Write your message..."
+            />
+          </div>
+          <div className={styles.contactFormGroup}>
+            <Reaptcha
+              sitekey={reCaptchaSiteKey}
+              onVerify={(response) => {
+                setVerified(true);
+                setResponse(response);
+              }}
+              onLoad={onLoad}
+            />
+          </div>
+          {response !== "" && (
+            <input type="hidden" name="g-recaptcha-response" value={response} />
+          )}
+          <div className={styles.contactFormGroup}>
+            <button type="submit" disabled={!loaded || !verified}>
+              Send
+            </button>
+          </div>
+        </form>
+      )}
+    </section>
+  );
+};
 
 interface ContactFormQuery {
   site: {
