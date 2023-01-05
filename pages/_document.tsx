b0f65713200ec58237e87e017239a242d9bf5936
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-05 23:03:50 +0200

    Move viewport meta to document, use <Title>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
                 <Favicons />
             </Head>
             <body>
