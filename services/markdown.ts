bff22aa0e8c6dc577b728c077eea534260851dc0
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-06 23:24:02 +0200

    fixup! Change remark-html to ReactMarkdown and gfm

--- services/markdown.ts
+++
@@ -1,9 +0,0 @@
-import { remark } from 'remark'
-import html from 'remark-html'
-
-type Html = string
-
-export const parseMarkdown = async (markdown: string): Promise<Html> => {
-    const processed = await remark().use(html).process(markdown)
-    return processed.toString('utf8')
-}
