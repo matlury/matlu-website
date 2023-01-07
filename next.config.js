66c8c19c082bb6c8b2b4ea858063d95499f65637
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-07 21:53:10 +0200

    Add shitload of security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 const path = require('path')
 
+const securityHeaders = [
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block',
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+    },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
@@ -11,6 +34,17 @@ const nextConfig = {
     },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+    },
+    poweredByHeader: false,
+    // CSP headers handled by middleware.ts
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/:path*',
+                headers: securityHeaders,
+            },
+        ]
     },
 }
 
