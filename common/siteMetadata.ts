4e4ae651494a167a891fa47f4be38dbf07d54b93
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-07 19:36:35 +0200

    Implement contact email form in next.js api route
    so we can get rid of the lambda to make the stack a bit more simple

--- common/siteMetadata.ts
+++ common/siteMetadata.ts
@@ -4,7 +4,6 @@ const siteMetadata = {
         'Helsingin yliopiston Matemaattis-luonnontieteellisten opiskelijajärjestöjen yhteistyöjärjestö Matlu',
     author: 'Matlu ry',
     recaptchaSiteKey: process.env.RECAPTCHA_SITE_KEY || '',
-    feedbackFormHandler: process.env.FEEDBACK_FORM_HANDLER_URL || '',
     googleSiteVerification: process.env.GOOGLE_SITE_VERIFICATION_KEY,
 }
 
---
+++ pages/thank-you.tsx
@@ -0,0 +1,32 @@
+import { withLayoutSSRProps } from 'common/withLayoutSSRProps'
+import Layout, { LayoutSSRProps } from 'components/Layout'
+import Link from 'next/link'
+
+const ThankYou = ({ locale, ...layoutProps }: LayoutSSRProps) => (
+    <Layout locale={locale} {...layoutProps}>
+        {locale === 'fi' ? (
+            <>
+                <h1>Kiitos yhteydenotostasi!</h1>
+                <p>Käsittelemme yhteydenotot mahdollisimman pian.</p>
+                <Link href="/">Takaisin etusivulle</Link>
+            </>
+        ) : (
+            <>
+                <h1>Thank you for contacting us!</h1>
+                <p>We will contact you as soon as possible.</p>
+                <Link href="/">Back to front page</Link>
+            </>
+        )}
+    </Layout>
+)
+
+export default ThankYou
+
+export const getServerSideProps = withLayoutSSRProps(async () => ({
+    props: {
+        seo: {
+            description: 'Kiitos yhteydenotostasi! / Thank you for contacting!',
+            hideFromSearchEngine: true,
+        },
+    },
+}))
