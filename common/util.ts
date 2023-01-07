eaf646e27e349a2fe874b05fd4c78a7b343f697f
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-07 16:10:06 +0200

    Implement events page template

--- common/util.ts
+++ common/util.ts
@@ -10,4 +10,27 @@ export const ascendingByString =
     (a: T, b: T) =>
         value(a).localeCompare(value(b))
 
+function omit<T extends object, K extends keyof T>(
+    key: K
+): (obj: T) => Omit<T, K>
+function omit<T extends object, K extends keyof T>(key: K, obj: T): Omit<T, K>
+function omit<T extends object, K extends keyof T>(
+    key: K,
+    obj?: T
+): Omit<T, K> | ((obj: T) => Omit<T, K>) {
+    if (typeof obj === 'undefined') {
+        return (o: T) => {
+            const shallowClone = { ...o }
+            delete shallowClone[key]
+            return shallowClone
+        }
+    }
+
+    const shallowClone = { ...obj }
+    delete shallowClone[key]
+    return shallowClone
+}
+
+export { omit }
+
 export const formatTitle = (title: string) => `${title} | Matlu ry`
