60601ef0125f6058293e925e4c8f59b885c008b7
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2023-01-07 20:44:31 +0200

    Add dockerfile for prod
    This commit also adds a docker-compose.yml file for testing the
    production build locally.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
+    output: 'standalone',
     reactStrictMode: true,
     i18n: {
         locales: ['en', 'fi'],
